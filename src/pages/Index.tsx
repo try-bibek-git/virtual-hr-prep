2d07fff90bf7032e0d57b54ce6bec36483e9bcfb
Bibek Biswas <jbiswas94236@gmail.com>
2025-05-30 15:07:47 +0530

    hero img update

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -161,7 +161,7 @@ const Index: React.FC = () => {
             </div>
             <div className="flex-1">
               <img 
-                src="/interview.png" 
+                src="/interview-dark.png" 
                 alt="Interview Illustration" 
                 className="w-full h-auto rounded-2xl shadow-lg" 
               />
